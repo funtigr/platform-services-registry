4d7a7abcff4028a4f97c03b98ada942554d09633
Artem Kravchenko <43115082+funtigr@users.noreply.github.com>
2024-05-16 16:29:56 -0700

    Update list.ts

--- app/api/private-cloud/products/_operations/list.ts
+++ app/api/private-cloud/products/_operations/list.ts
@@ -1,12 +1,10 @@
-import { Session } from 'next-auth';
 import prisma from '@/core/prisma';
 
-export default async function listOp({ session }: { session: Session }) {
+export default async function listOp() {
   const products = await prisma.privateCloudProject.findMany({
     where: {},
     skip: 0,
     take: 2,
-    session: session as never,
   });
   return products;
 }
